80419c5f74c40b850dc3f5015f785945c801be98
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-05-02 12:37:52 +0800

    Fixed the navbar for mobile screen.

--- src/shared/components/AppNavbar.tsx
+++ src/shared/components/AppNavbar.tsx
@@ -6,17 +6,22 @@ import styles from './AppNavbar.module.scss'
 
 const AppNavbar: React.FC = () => {
   return (
-    <Navbar className={styles['all-caps']} bg="light" variant="light">
+    <Navbar className={styles['all-caps']} bg="light" variant="light" expand="lg">
       <Navbar.Brand href="/">
         <h1>Ariel <em>&amp;</em> Phebe</h1>
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Link className="nav-link" to="/">Home</Link>
-        {/*<Nav.Link className={styles['all-caps']} href="/groom-and-bride">Groom <em>&amp;</em> Bride</Nav.Link>*/}
-        {/*<Nav.Link className={styles['all-caps']} href="/when-and-where">When <em>&amp;</em> Where</Nav.Link>*/}
-        <Link className="nav-link" to="/rsvp">Rsvp</Link>
-        <Link className="nav-link" to="/social">Social</Link>
-      </Nav>
+
+      <Navbar.Toggle aria-controls="app-navbar-links"/>
+
+      <Navbar.Collapse id="app-navbar-links">
+        <Nav className="mr-auto">
+          {/*<Nav.Link className={styles['all-caps']} href="/groom-and-bride">Groom <em>&amp;</em> Bride</Nav.Link>*/}
+          {/*<Nav.Link className={styles['all-caps']} href="/when-and-where">When <em>&amp;</em> Where</Nav.Link>*/}
+          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/rsvp">Rsvp</Link>
+          <Link className="nav-link" to="/social">Social</Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
